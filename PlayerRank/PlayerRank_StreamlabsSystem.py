18c4c7092bf8f86c7644c62f3002546a6e87a775
Chris Diesch <cdiesch@users.noreply.github.com>
2018-09-04 22:39:36 -0600

    Added script for loading player information
    Added a script for loading any player's ranked information (currently does not support all platforms)

---
+++ PlayerRank/PlayerRank_StreamlabsSystem.py
@@ -0,0 +1,128 @@
+import sys
+import clr
+import os
+import json
+
+rank_prog = os.path.join(os.path.abspath(os.path.split(__file__)[0]), 'get-rank.py')
+
+clr.AddReference('IronPython.SQLite.dll')
+clr.AddReference('IronPython.Modules.dll')
+
+ScriptName = 'PlayerRank'
+Website = 'http://wwww.twitch.tv/TheKingSalamander'
+Description = 'Gets the smite rank of a given player.'
+Creator = 'Chris Diesch'
+Version = '1.0.1'
+
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari'
+                     '/537.11'}
+
+# Settings tags
+platform_tag = 'platform'
+mode_tag = 'gameMode'
+format_tag = 'responseStr'
+cmd_tag = 'command'
+
+# Response format keys
+ign_key = '$ign'
+mode_key = '$mode'
+div_key = '$division'
+tp_key = '$tp'
+elo_key = '$elo'
+url_key = '$sg_page'
+user_tag = '$username'
+win_tag = '$wins'
+loss_tag = '$losses'
+wr_tag = '$winrate'
+matches_tag = '$matches'
+
+# The default settings
+settings = {platform_tag: 'PC',
+            mode_tag: 'Ranked: Conquest',
+            format_tag: '@$username $ign\'s Rank in $mode: $division ($tp TP) Smite Guru elo: $elo '
+                        '(more info $sg_page)',
+            cmd_tag: '!rank'}
+
+# the path to the settings file
+settings_path = os.path.join(os.path.join(os.path.dirname(__file__), 'settings'))
+settings_file = os.path.join(settings_path, 'SmiteRank_settings.json')
+
+
+# Init definition
+def Init():
+    # load the settings from the settings file if it exists
+    global settings
+    try:
+        if not os.path.exists(settings_path):
+            os.makedirs(settings_path)
+        if os.path.exists(settings_file):
+            with open(settings_file) as reader:
+                new_settings = json.load(reader)
+                if len(new_settings) != 0:
+                    settings = new_settings
+        # The command MUST start with '!'
+        if not settings[cmd_tag].startswith('!'):
+            settings[cmd_tag] = '!%s' % settings[cmd_tag]
+    except Exception as ex:
+        sys.stderr.write('Unable to load settings from file!\n'
+                         'Exception: %s' % str(ex))
+
+
+# Execute definition
+def Execute(data):
+    # Is this a chat message and is it the !rank command?
+    if data.IsChatMessage() and data.GetParam(0).lower() == settings[cmd_tag]:
+        info = {}
+        if data.GetParamCount() == 2:
+            ign = data.GetParam(1)
+            try:
+                message = ''
+                cmd = r'C:\Python27\python.exe "%s" "%s" "%s" --platform "%s"' % \
+                      (rank_prog,
+                       ign,
+                       settings[mode_tag],
+                       settings[platform_tag])
+                with os.popen(cmd) as out:
+                    message += out.read().replace(os.linesep, '')
+                info = json.loads(message)
+
+                message = settings[format_tag]. \
+                    replace(user_tag, data.User). \
+                    replace(ign_key, ign).\
+                    replace(mode_key, settings[mode_tag]).\
+                    replace(div_key, info['division']).\
+                    replace(tp_key, info['tp']).\
+                    replace(elo_key, info['elo']).\
+                    replace(url_key, info['url']).\
+                    replace(win_tag, info['win']).\
+                    replace(loss_tag, info['loss']).\
+                    replace(wr_tag, info['wr']).\
+                    replace(matches_tag, info['matches'])
+                # print(message)
+                Parent.SendTwitchMessage(message)
+            except Exception as ex:
+                Parent.SendTwitchMessage('@%s Failed to load data for user %s' % (data.User, ign))
+                raise Exception('Error: Unable to load data for user\n'
+                                '  Message: %s\n'
+                                '  Data:\n'
+                                '%s' % (str(ex), json.dumps(info, indent=3)))
+        else:
+            Parent.SendTwitchMessage('The %s command was not entered properly, the format is %s [IGN]' %
+                                     (settings[cmd_tag], settings[cmd_tag]))
+
+
+# ReloadSettings definition
+def ReloadSettings(json_data):
+    # Update the settings from stream labs chat bot UI
+    global settings
+    settings = json.loads(json_data)
+    # The command MUST start with '!'
+    if not settings[cmd_tag].startswith('!'):
+        settings[cmd_tag] = '!%s' % settings[cmd_tag]
+    with open(settings_file, 'w+') as writer:
+        json.dump(settings, writer)
+
+
+# Tick definition (does nothing)
+def Tick():
+    return
---
+++ PlayerRank/get-rank.py
@@ -0,0 +1,76 @@
+import re
+import sys
+import json
+import urllib
+import argparse
+import requests
+import datetime
+import BeautifulSoup
+
+_name = 'SmiteRank'
+_usage = 'get-rank.py [options...] [IGN] [MODE]'
+_description = 'Gets the smite rank of the streamer'
+_author = 'Chris Diesch'
+_version = '1.0.0'
+
+_parser = argparse.ArgumentParser(prog=_name, usage=_usage, description=_description)
+
+_parser.add_argument('ign')
+_parser.add_argument('mode')
+_parser.add_argument('-p', '--platform', default='PC', choices=['PC', 'Xbox', 'Playstation'])
+
+_ARGS = None
+
+command = '!rank'
+IGN_TO_SG = 'http://smite.guru/profile/$platform/$player_name/ranked'
+PLATFORM_MAPPING = {'PC': "pc", 'Xbox': 'xb', 'Playstation': 'ps'}
+RANK_REGEX = r'.*%(?P<division>[A-Za-z ]*)' \
+             r'(?P<elo>[0-9,]*)Elo' \
+             r'(?P<tp>[0-9]*)TP' \
+             r'(?P<win>[0-9]*)W' \
+             r'(?P<loss>[0-9]*L)' \
+             r'(?P<wr>[0-9.]*)%' \
+             r'(?P<matches>[0-9]*).*'
+
+
+def load_ranked_info():
+    # convert the player name and platform to the url required ones (MUST ENSURE THE NAME IS URL SAFE!)
+    platform_url = PLATFORM_MAPPING[_ARGS.platform]
+    ign_url = urllib.quote(_ARGS.ign, safe='')
+    # create the necessary url
+    sg_url = IGN_TO_SG.replace('$platform', platform_url).replace('$player_name', ign_url)
+    try:
+        # data = urllib2.Request(sg_url, headers=hdr)
+        # page = BeautifulSoup.BeautifulSoup(urllib2.urlopen(data))
+        page = BeautifulSoup.BeautifulSoup(requests.get(sg_url).text)
+        result = parse_page(page)
+        result['url'] = sg_url
+    except Exception as ex:
+        time_stamp = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        result = 'Unable to load smite guru data for %s (Check here: %s)' % (_ARGS.ign, sg_url)
+        sys.stderr.write('%s Unable to load SmiteGuru data!\n'
+                         '   Exception: %s\n'
+                         '   URL: "%s"' % (time_stamp, str(ex), sg_url))
+    # convert the dict to a json string
+    return json.dumps(result)
+
+
+def parse_page(page):
+    rank_data = ''
+    results = page.findAll('div', {'class': 'widget'})
+    for result in results:
+        text = str(result.text)
+        if _ARGS.mode in text:
+            rank_data = text.replace(_ARGS.mode, '')
+            break
+    info = re.match(RANK_REGEX, rank_data)
+    if info is not None:
+        info = info.groupdict()
+    else:
+        info = {}
+    return info
+
+if __name__ == '__main__':
+    _ARGS = _parser.parse_args()
+    print(load_ranked_info())
+
